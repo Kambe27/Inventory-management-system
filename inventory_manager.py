b5307e7ca8f42de1da3ed0be225f1632913dce1d
Kambe27 <gautamsinghsaini2@gmail.com>
2026-05-22 18:20:43 +0800

    Added CLI

--- inventory_manager.py
+++ inventory_manager.py
@@ -1,5 +1,72 @@
 #this python file handles the inventory management of the store, including adding new items, updating stock levels, and removing items from the inventory.
 
 import json
-import csv
 import os
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    
+    
+def display_menu():
+    print("Inventory Management System")
+    print("1. Add New Item")
+    print("2. Update Stock Levels")
+    print("3. Remove Item")
+    print("4. View Inventory")
+    print("5. Bulk Import from CSV")
+    print("6. Exit")
+    
+
+def load_inventory():
+    print("Loading inventory...")
+    
+    return{}
+
+def save_inventory(inventory):
+    print("Saving inventory...")    
+    
+    
+def add_new_item(inventory):
+    print("Add new item to inventory")
+    
+def update_stock_levels(inventory):
+    print("Update stock levels")
+    
+def remove_item(inventory):
+    print("Remove item from inventory")     
+    
+def view_inventory(inventory):
+    print("Current inventory:")
+    
+def bulk_import(inventory):
+    print("Bulk import from CSV")
+    
+def main():
+    clear_screen()
+    inventory = load_inventory()
+    
+    while True:
+        print("\n")
+        display_menu()
+        choice = input("Enter your choice: ").strip()
+        
+        clear_screen()
+        if choice == '1':
+            add_new_item(inventory)
+        elif choice == '2':
+            update_stock_levels(inventory)
+        elif choice == '3':
+            remove_item(inventory)
+        elif choice == '4':
+            view_inventory(inventory)
+        elif choice == '5':
+            bulk_import(inventory)
+        elif choice == '6':
+            save_inventory(inventory)
+            print("Exiting Inventory Management System. Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+            
+if __name__ == "__main__":
+    main()
